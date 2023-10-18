6e2705239b0c3ba676367a744435c122610dfe0f
SimonUlloa <simon_ulloa@outlook.com>
2023-10-17 22:56:23 -0400

    Add StudentHome.html and fixed post-auth

--- api/src/functions/post-auth.js
+++ api/src/functions/post-auth.js
@@ -8,8 +8,10 @@ app.http('post-auth', {
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log('Processing a login request.');
+        context.log('HTTP request:', request);
 
-        const email = request.body.email;
+        const body = JSON.parse(await request.text())
+        const email = body.email
 
         if (!email) {
             return { status: 400, body: email };
@@ -20,32 +22,56 @@ app.http('post-auth', {
             host: 'bitsem.mysql.database.azure.com',
             user: 'clay',
             password: 'Bit44542023',
-            database: 'main'
+            database: 'main',
+            port: 3306,
+            ssl: true
         });
 
         db.connect();
 
         try {
+            var ProfessorID = null;
+            var StudentID = null;
+            var firstName = null;
             // Check if the email is present in the student or professor table
-            let query = 'SELECT FirstName FROM student WHERE email = ? UNION SELECT FirstName FROM professor WHERE email = ?';
+            let query = 'SELECT FirstName,StudentID FROM student WHERE email = ?';
             let [rows, fields] = await new Promise((resolve, reject) => {
-                db.query(query, [email, email], function (error, results) {
+                db.query(query, [email], function (error, results) {
                     if (error) return reject(error);
                     resolve(results);
                 });
             });
-
-            if (!rows.length) {
-                return { status: 404, body: "Email not found" };
+            // If the email is present in the student table, get the student ID
+            if (rows != undefined) {
+                StudentID = rows.StudentID;
+                firstName = rows.FirstName;
             }
 
-            const firstName = rows[0].FirstName;
+            query = 'SELECT FirstName,ProfessorID FROM professor WHERE email = ?';
+            let [rowsP, fieldsP] = await new Promise((resolve, reject) => {
+                db.query(query, [email], function (error, results) {
+                    if (error) return reject(error);
+                    resolve(results);
+                });
+            });
+            // If the email is present in the student table, get the student ID
+            if (rowsP != undefined) {
+                ProfessorID = rowsP.ProfessorID;
+                firstName = rowsP.FirstName;
+            }
+            // If the email is not present in either table, return an error
+            if (!ProfessorID && !StudentID || ProfessorID && StudentID) {
+                return {
+                    status: 200,
+                    body: "Nothing found"
+                };
+            }
             const sessionId = uuidv4(); // Generate a unique session ID
 
             // Store session in the database
-            query = 'INSERT INTO sessions (session_id, created_at) VALUES (?, NOW())';
+            query = 'INSERT INTO sessions (session_id, student_id, professor_id, created_at) VALUES (?, ?, ?, NOW())';
             await new Promise((resolve, reject) => {
-                db.query(query, [sessionId], function (error, results) {
+                db.query(query, [sessionId, StudentID, ProfessorID], function (error, results) {
                     if (error) return reject(error);
                     resolve(results);
                 });
@@ -54,7 +80,7 @@ app.http('post-auth', {
             // Construct the payload for the external service
             const payload = {
                 email: email,
-                link: `https://zealous-plant-073f3f80f.azurestaticapps.net/?session=${sessionId}`,
+                link: `https://zealous-plant-073f3f80f.4.azurestaticapps.net/${StudentID != undefined ? "StudentHome" : "ProfessorHome"}?session=${sessionId}`,
                 name: firstName
             };
 
@@ -68,7 +94,7 @@ app.http('post-auth', {
         } catch (error) {
             context.log(error);
             return {
-                status: 500,
+                status: 200,
                 body: `Error processing request: ${error.message}`
             };
         } finally {
