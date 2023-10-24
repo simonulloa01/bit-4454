5277032328e7f3db4dc6ca8d29370d9d82c8df0f
Simon Ulloa <41999304+simonulloa01@users.noreply.github.com>
2023-10-23 22:12:09 -0400

    updates roster

--- api/src/functions/upload-roster.js
+++ api/src/functions/upload-roster.js
@@ -1,7 +1,7 @@
 const { app } = require('@azure/functions');
 const mysql = require('mysql');
 
-app.http('upload-roster', {
+app.http('upload-json', {
     methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
@@ -9,7 +9,8 @@ app.http('upload-roster', {
 
         // Parse JSON body from the request
         const payload = JSON.parse(await request.text());
-        const session_id = payload.session_id; // Assuming session_id is sent as part of the JSON payload
+        const session_id = payload.session_id; // Professor's session_id for authentication
+        const section_id = payload.section_id; // Section ID for the specific course section
         const students = payload.students; // Assuming the JSON contains an array of students
 
         // Setup your SQL connection
@@ -25,22 +26,40 @@ app.http('upload-roster', {
         db.connect();
 
         try {
-            // Loop through the students and add them to the database
+            // First, validate the professor's session_id if necessary
+            // This step depends on your authentication logic and database schema
+            let authQuery = 'SELECT * FROM sessions WHERE session_id = ?'; // Adjust based on your schema
+            let session = await new Promise((resolve, reject) => {
+                db.query(authQuery, [session_id], (error, results) => {
+                    if (error) return reject(error);
+                    resolve(results[0]); // assuming the session is unique and only one record will be returned
+                });
+            });
+
+            // Check if the session exists (i.e., the professor is authenticated)
+            if (!session) {
+                return {
+                    status: 401,
+                    body: 'Unauthorized: Invalid session ID'
+                };
+            }
+
+            // If authenticated, proceed to process the student data
             for (let student of students) {
-                // Perform the necessary query for each student
-                // Make sure to handle SQL injection by escaping the input values
-                let query = 'INSERT INTO student (LastName, FirstName, GroupId, Email) VALUES (?, ?, ?, ?)';
-                await new Promise((resolve, reject) => {
-                    db.query(query, [student.LastName, student.FirstName, student.GroupId, student.Email], (error, results, fields) => {
+                // Add student to the 'student' table
+                let studentQuery = 'INSERT INTO student (LastName, FirstName, GroupId, Email) VALUES (?, ?, ?, ?)';
+                let studentInsertResult = await new Promise((resolve, reject) => {
+                    db.query(studentQuery, [student.LastName, student.FirstName, student.GroupId, student.Email], (error, results) => {
                         if (error) return reject(error);
                         resolve(results);
                     });
                 });
 
-                // Assuming there's a relationship between session and student
-                query = 'INSERT INTO session_students (session_id, student_id) VALUES (?, ?)';
+                // If necessary, use the result of the insert operation (like the inserted ID) for further queries
+                // For example, if you need to link the student with the section
+                let studentSectionQuery = 'INSERT INTO student_section (student_id, section_id) VALUES (?, ?)';
                 await new Promise((resolve, reject) => {
-                    db.query(query, [session_id, student.StudentID], (error, results, fields) => { // Assuming student.StudentID is available
+                    db.query(studentSectionQuery, [studentInsertResult.insertId, section_id], (error, results) => {
                         if (error) return reject(error);
                         resolve(results);
                     });
