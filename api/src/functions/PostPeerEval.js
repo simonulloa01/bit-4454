ab3416b9dae9aa4966cd691ff2edb6e04d7a5d95
SimonUlloa <simon_ulloa@outlook.com>
2023-11-29 22:57:12 -0500

    Add PostPeerEval function and datepicker functionality

---
+++ api/src/functions/PostPeerEval.js
@@ -0,0 +1,56 @@
+const mysql = require('mysql');
+const axios = require('axios');
+const { v4: uuidv4 } = require('uuid'); // for generating session UUID
+const { app } = require('@azure/functions');
+
+app.http('PostPeerEval', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+        context.log('Processing a post peer eval request.');
+        context.log('HTTP request:', request);
+
+        const body = JSON.parse(await request.text())
+        // const session_id = body.session_id
+        const DueDate = body.DueDate;
+        const CourseID = body.CourseID;
+
+        if (!session_id) {
+            return { status: 400, body: session_id };
+        }
+
+        // Setup your SQL connection
+        const db = mysql.createConnection({
+            host: 'bitsem.mysql.database.azure.com',
+            user: 'clay',
+            password: 'Bit44542023',
+            database: 'main',
+            port: 3306,
+            ssl: true
+        });
+
+        db.connect();
+        //Try to post to the schedule table (ScheduleID, DueDate, CourseID) 
+        try {
+            const query = `
+            INSERT INTO schedule (ScheduleID, DueDate, CourseID) VALUES (?, ?, ?)
+            `;
+            const queryData = [
+                body.ScheduleID,
+                body.DueDate,
+                body.CourseID
+            ];
+            let [rows, fields] = await new Promise((resolve, reject) => {
+                db.query(query, queryData, function (error, results) {
+                    if (error) return reject(error);
+                    resolve(results);
+                });
+            });
+            return { status: 200, body: rows };
+        } catch (error) {
+            context.log(error);
+            return { status: 500, body: error };
+        }
+    }
+});
